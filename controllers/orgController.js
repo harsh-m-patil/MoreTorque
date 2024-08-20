362b0cbadf9f520f16d647f4e2afe1d71efcdb0c
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-08-20 20:29:39 +0530

    implement update propagation

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -80,30 +80,77 @@ exports.getOrg = async (req, res, next) => {
   }
 };
 
-exports.updateOrg = async (req, res, next) => {
-  try {
-    const updatedOrg = await Org.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+const propagatePolicies = async (org, updates) => {
+  const childrenOrgs = await Org.find({ parentOrg: org._id });
 
-    if (!updatedOrg) {
-      return res.status(404).json({
-        status: "fail",
-        message: "No Org with that id found",
-      });
+  if (!childrenOrgs) {
+    return;
+  }
+
+  childrenOrgs.forEach(async (child) => {
+    let updateChild = false;
+    // update all childs
+    if (updates.fuelReimbursementPolicy) {
+      child.fuelReimbursementPolicy = updates.fuelReimbursementPolicy;
+      updateChild = true;
     }
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        updatedOrg,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: "Error updating Org",
-    });
+    // update those childs who do not have a speedLimitPolicy
+    if (updates.speedLimitPolicy && !child.speedLimitPolicy) {
+      child.speedLimitPolicy = updates.speedLimitPolicy;
+      updateChild = true;
+    }
+
+    // if child is update its child
+    if (updateChild) {
+      await child.save();
+      await propagatePolicies(child, updates);
+    }
+  });
+};
+
+exports.updateOrg = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    const org = await Org.findById(id).populate("parentOrg");
+
+    if (!org) {
+      return res.status(404).send({ error: "Organization not found" });
+    }
+
+    if (updates.fuelReimbursementPolicy) {
+      // check if fuel policy is set by parent
+      if (
+        org.parentOrg &&
+        org.fuelReimbursementPolicy === org.parentOrg.fuelReimbursementPolicy
+      ) {
+        return res.status(400).send({
+          error:
+            "Cannot update inherited fuelReimbursementPolicy, update at the parent level",
+        });
+      }
+      // else update
+      org.fuelReimbursementPolicy = updates.fuelReimbursementPolicy;
+    }
+
+    // update no restrictions
+    if (updates.speedLimitPolicy) {
+      org.speedLimitPolicy = updates.speedLimitPolicy;
+    }
+
+    if (updates.account) org.account = updates.account;
+    if (updates.website) org.website = updates.website;
+
+    await org.save();
+
+    if (updates.fuelReimbursementPolicy || updates.speedLimitPolicy) {
+      await propagatePolicies(org, updates);
+    }
+
+    res.status(200).send(org);
+  } catch (error) {
+    res.status(500).send({ error: "Internal Server Error" });
   }
 };
