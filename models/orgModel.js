2d37c272372f6461ff14d6a057ea805630a3c944
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-08-20 15:06:35 +0530

    modify org model

--- models/orgModel.js
+++ models/orgModel.js
@@ -1,14 +1,31 @@
 const mongoose = require("mongoose");
 
 const orgSchema = new mongoose.Schema({
-  orgName: {
+  name: {
     type: String,
     required: [true, "Organization name is required"],
     unique: [true, "This organization name is already in use"],
+    minLength: [4, "OrgName can be minimum of 4 length"],
+    maxlength: [40, "OrgName can be maximum of 40 length"],
   },
   parentOrg: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Org",
+  },
+  account: {
+    type: String,
+    required: [true, "Account is required"],
+  },
+  website: {
+    type: String,
+  },
+  fuelReimbursementPolicy: {
+    type: String,
+    default: "1000",
+  },
+  speedLimitPolicy: {
+    type: String,
+    required: [true, "Speed limit policy is required"],
   },
   //childrenOrgs: [
   //  {
