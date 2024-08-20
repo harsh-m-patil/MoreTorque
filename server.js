91cc7c8f84f6e25760e346a0f6bcf77aac5cd755
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-08-20 13:29:46 +0530

    add rate limiting to vehicle endpoint

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const rateLimit = require("express-rate-limit");
 const vehicleRouter = require("./routes/vehicleRouter");
 
 dotenv.config();
 
 const app = express();
 
+const limiter = rateLimit({
+  max: 5,
+  windowMs: 60 * 1000,
+  message: "Too many Requests from this IP,try again in a minute",
+});
+
+app.use("/vehicle", limiter);
 app.use("/vehicle", vehicleRouter);
 
 app.get("/", (req, res) => {
