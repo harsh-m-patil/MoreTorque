adc2e2bd401a15a7deeb3c516d5af65f25cdcbe9
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-08-20 17:18:53 +0530

    add logging

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const morgan = require("morgan");
 
 const vehicleRouter = require("./routes/vehicleRouter");
 const orgRouter = require("./routes/orgRouter");
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+);
+
+app.use(morgan("dev"));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP! Try again in an hour",
+});
+
+app.use(limiter);
 
 app.use("/vehicle", vehicleRouter);
 app.use("/org", orgRouter);
